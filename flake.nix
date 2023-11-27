85ec235018baf8621e6ee14fd54f786dc84013ba
Márton Boros <martonboros@gmail.com>
2023-11-27 11:44:57 +0200

    nix build wip

--- flake.nix
+++ flake.nix
@@ -4,15 +4,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    naersk.url = "github:nix-community/naersk/master";
   };
 
   outputs = inputs@{ self, flake-parts, nixpkgs, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
-    perSystem = { pkgs, ... }: {
-      packages = rec {
-        default = godot_4;
-        godot_4 = pkgs.godot_4;
+    perSystem = { pkgs, ... }:
+      let naersk-lib = pkgs.callPackage inputs.naersk { };
+      in
+      {
+        packages = rec {
+          default = godot_4;
+          godot_4 = pkgs.godot_4;
+          libcsl_godot = naersk-lib.buildPackage {
+            src = ./libcsl_godot;
+            copyLibs = true;
+          };
+        };
       };
-    };
     systems = [ "x86_64-linux" ];
   };
 }
