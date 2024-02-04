bf68f1a6074e452609860920983061775090d3d9
Daniel Farrelly <daniel@mlabs.city>
2024-02-04 22:06:33 +0000

    Add CBOR error enum

--- addons/@mlabs-haskell/godot-cardano/src/cbor.gd
+++ addons/@mlabs-haskell/godot-cardano/src/cbor.gd
@@ -2,6 +2,19 @@ extends RefCounted
 
 class_name Cbor
 
+enum Status {
+	SUCCESS = 0,
+	DECODING_INVALID_INT = 1,
+	DECODING_INVALID_BYTES = 2,
+	DECODING_INVALID_CONSTR = 3,
+	DECODING_INVALID_TAG = 4,
+	DECODING_UNSUPPORTED_TYPE = 5,
+	ENCODING_INVALID_TAG = 6,
+	ENCODING_UNKNOWN_OBJECT = 7,
+	ENCODING_UNSUPPORTED_TYPE = 8,
+	CBOR_EVENT_ERROR = 9,
+}
+
 class DeserializeResult extends Result:
 	## WARNING: This function may fail! First match on [Result_.tag] or call [Result_.is_ok].
 	var value: Variant:
